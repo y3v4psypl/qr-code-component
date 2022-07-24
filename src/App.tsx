55a345522841b812ee7fd0d8164069ec380a30fb
Eva Vetrova <yevavetrova@gmail.com>
2022-07-24 03:32:12 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from "./images/image-qr-code.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="main">
+          <div className="box">
+              <img src={Image} alt="QR-code"/>
+              <p id="p1">Improve your front-end skills by building projects</p>
+              <p id="p2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          </div>
+      </div>
   );
 }
 
